7a7f90fa6d64de803da42a787ae46e81e1c07085
Cameron Smith <smithc11@rpi.edu>
2019-04-23 18:19:40 -0400

    pumi with simmodsuite

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 spack install gcc@7.4.0%gcc@centos7_4.8.5
 spack install mpich%gcc@7.4.0 +verbs +slurm pmi=pmi
+spack install pumi@develop +shared +fortran +zoltan simmodsuite=full ^/z2g2n5c ^/3dnrzjl ^zoltan+parmetis ^simmodsuite@12.0-190413
